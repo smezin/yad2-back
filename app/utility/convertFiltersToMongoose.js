f47995521b6a7c32fa44871b56cc334964119556
Shahaf Mezin <smezin@gmail.com>
2020-11-12 23:54:20 +0200

    some more filters are wired to advanced search

--- app/utility/convertFiltersToMongoose.js
+++ app/utility/convertFiltersToMongoose.js
@@ -3,16 +3,64 @@ const convertFiltersToMongoose = (filters) => {
     masterCategory: "realestate",
     $and: [
       {
-        $and: [
-          {price: {$lte: filters.maxPrice || Infinity }},
-          {price: {$gte: filters.minPrice || 0}}
+        $or: [
+          {
+            $and: [
+              {floor: {$lte: filters.maxFloor || Infinity }},
+              {floor: {$gte: filters.minFloor || 0}}
+            ]
+          },
+          {
+            floor: {$exists: false}
+          }
+        ]
+        
+      },
+      {
+        $or: [
+          {$and: [
+            {price: {$lte: filters.maxPrice || Infinity }},
+            {price: {$gte: filters.minPrice || 0}}
+          ]},
+          {
+            price: {$exists: false}
+          }
+        ]        
+      },
+      {
+        $or: [
+          {
+            $and: [
+              {rooms: {$lte: filters.maxRooms || Infinity}},
+              {rooms: {$gte: filters.minRooms || 0}}
+            ]
+          },
+          {
+            rooms: {$exists: false}
+          }
+        ]        
+      },
+      {
+        $or: [
+          {
+            $and: [
+              {size: {$lte: filters.maxSize || Infinity }},
+              {size: {$gte: filters.minSize || 0}}
+            ]
+          },
+          {
+            size: {$exists: false}
+          }
+        ]        
+      },
+      {
+        $or: [
+          {propertyType: {$in: filters.propertyTypes}},
+          {propertyType: {$exists: !filters.propertyTypes}}
         ]
       },
       {
-        $and: [
-          {rooms: {$lte: filters.maxRooms || Infinity}},
-          {rooms: {$gte: filters.minRooms || 0}}
-        ]
+        category: {$eq: filters.category}
       }
     ]
     
